973477742f441449cce51b5f4a55953a0356539f
Dmytro Stelmakh <darknow04251995@gmail.com>
2020-09-04 16:19:53 +0300

    added hw10.py

---
+++ hw10.py
@@ -0,0 +1,13 @@
+# Will there be enough space?:
+
+def enough(cap, on, wait):
+    cap = int(cap)
+    on = int(on)
+    wait = int(wait)
+    
+    if cap >= on + wait:
+        return 0
+    else:
+        diff = on + wait - cap
+        cap != on + wait
+        return diff
