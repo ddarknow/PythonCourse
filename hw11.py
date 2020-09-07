b0a3a599ea493572a606c4aafa54a55bbf911f81
Dmytro Stelmakh <darknow04251995@gmail.com>
2020-09-07 12:51:31 +0300

    add hw11

---
+++ hw11.py
@@ -0,0 +1,7 @@
+#Banjo:
+
+def areYouPlayingBanjo(name):
+    if list(name)[0] == 'R' or list(name)[0] == 'r':
+        return f"{name} plays banjo"
+    else:
+        return f"{name} does not play banjo"
