ca284aab5109492fb14f4cb931db11c15c01e01d
Dmytro Stelmakh <darknow04251995@gmail.com>
2020-09-08 14:25:37 +0300

    added hw12-14

---
+++ hw13.py
@@ -0,0 +1,4 @@
+# Counting Sheep:
+
+def count_sheeps(sheep):
+    return sheep.count(True)
---
+++ hw14.py
@@ -0,0 +1,7 @@
+#That tail Comparison:
+
+def correct_tail(body, tail):
+    if body[-1] == tail[0]:
+        return True
+    else:
+        return False
